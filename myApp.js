1695a9c848cf28c6c390f0ef2563e2505c2ea3f8
myskillsdream <myskillsdream@gmail.com>
2022-07-20 21:57:49 +0100

    create model

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {dbName: "cluster0"}, {useNewUrlParser: true}); 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}); 
 
 var Schema = mongoose.Schema;
 
