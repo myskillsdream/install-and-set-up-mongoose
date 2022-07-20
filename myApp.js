b51d7d2d58282406971d8030bcae5b951c61d2a6
myskillsdream <myskillsdream@gmail.com>
2022-07-20 12:59:15 +0100

    create mongodb schema

--- myApp.js
+++ myApp.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-let mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI); 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, {useUnifiedTopology: true}); 
 
-const Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
-const personSchema = new Schema({
+var personSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String]
 });
 
-let Person = mongoose.model("Person", personSchema);
+var Person = mongoose.model("Person", personSchema);
 
 console.log(Person)
 
