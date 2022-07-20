1d54c2a1ff200c54a49bb0c8f5623bc3bae07014
myskillsdream <myskillsdream@gmail.com>
2022-07-20 22:18:45 +0100

    create model

--- myApp.js
+++ myApp.js
@@ -28,57 +28,57 @@ let ijabor = new Person({
 
 // let Person;
 
-// const createAndSavePerson = (done) => {
-//   done(null /*, data*/);
-// };
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
 
-// const createManyPeople = (arrayOfPeople, done) => {
-//   done(null /*, data*/);
-// };
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
 
-// const findPeopleByName = (personName, done) => {
-//   done(null /*, data*/);
-// };
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
 
-// const findOneByFood = (food, done) => {
-//   done(null /*, data*/);
-// };
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
 
-// const findPersonById = (personId, done) => {
-//   done(null /*, data*/);
-// };
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
 
-// const findEditThenSave = (personId, done) => {
-//   const foodToAdd = "hamburger";
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
 
-//   done(null /*, data*/);
-// };
+  done(null /*, data*/);
+};
 
-// const findAndUpdate = (personName, done) => {
-//   const ageToSet = 20;
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
 
-//   done(null /*, data*/);
-// };
+  done(null /*, data*/);
+};
 
-// const removeById = (personId, done) => {
-//   done(null /*, data*/);
-// };
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
 
-// const removeManyPeople = (done) => {
-//   const nameToRemove = "Mary";
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
 
-//   done(null /*, data*/);
-// };
+  done(null /*, data*/);
+};
 
-// const queryChain = (done) => {
-//   const foodToSearch = "burrito";
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
 
-//   done(null /*, data*/);
-// };
+  done(null /*, data*/);
+};
 
-// /** **Well Done !!**
-// /* You completed these challenges, let's go celebrate !
-//  */
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
 
 //----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
 
