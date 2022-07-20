a814e8febb5f79632a2abdbe4e7420ff839470fe
myskillsdream <myskillsdream@gmail.com>
2022-07-20 21:38:37 +0100

    create model

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}, {useUnifiedTopology: true}); 
 
 var Schema = mongoose.Schema;
@@ -9,17 +10,19 @@ var personSchema = new Schema({
   name: { type: String, required: true },
   age: Number,
   favoriteFoods: [String]
-});
+}
+);
 
 var Person = mongoose.model("Person", personSchema);
 
-console.log(Person)
+// console.log(Person)
 
 let ijabor = new Person({
   name: "Ijabor",
   age: 32,
   favoriteFoods: ["rice", "beans"]
-});
+}
+);
 
 // let Person;
 
