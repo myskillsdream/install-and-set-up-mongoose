78095627d0b61d8847f179953163506d883ece52
myskillsdream <myskillsdream@gmail.com>
2022-07-22 20:52:34 +0100

    add person object to mongodb

--- myApp.js
+++ myApp.js
@@ -76,8 +76,11 @@ var findPeopleByName = function(personName, done) {
   });
 };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+var findOneByFood = function(food, done) {
+  Person.findOne({favoriteFoods: food}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
