75fa80d829d2f0b009e254320966cbb47aac59e6
myskillsdream <myskillsdream@gmail.com>
2022-07-22 21:22:39 +0100

    add person object to mongodb

--- myApp.js
+++ myApp.js
@@ -97,7 +97,7 @@ const findEditThenSave = (personId, done) => {
     if(err) return console.log(err); 
   
     person.favoriteFoods.push(foodToAdd);
-    
+
     person.save((err, updatedPerson) => {
       if(err) return console.log(err);
       done(null, updatedPerson)
@@ -108,7 +108,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+    if(err) return console.log(err);
+    done(null, updatedDoc);
+  })
 };
 
 const removeById = (personId, done) => {
